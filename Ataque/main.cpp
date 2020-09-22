6fcf850bafd7fb6dfe4c93967a1c9a58e8f17667
Matt <a01422885@itesm.mx>
2020-09-22 15:23:16 -0500

    Initial Commit

---
+++ Ataque/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ataque
+//
+//  Created by Matías Méndez on 22/09/20.
+//  Copyright © 2020 Matías Méndez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
