a53b186799342945dc2b912634dd8f3496ba614d
Matt <a01422885@itesm.mx>
2020-10-11 22:52:08 -0500

    prueba

---
+++ Ataque/ConexionesComputadoras.cpp
@@ -0,0 +1,9 @@
+//
+//  ConexionesComputadoras.cpp
+//  Ataque
+//
+//  Created by Matías Méndez on 11/10/20.
+//  Copyright © 2020 Matías Méndez. All rights reserved.
+//
+
+#include "ConexionesComputadoras.hpp"
---
+++ Ataque/ConexionesComputadoras.hpp
@@ -0,0 +1,14 @@
+//
+//  ConexionesComputadoras.hpp
+//  Ataque
+//
+//  Created by Matías Méndez on 11/10/20.
+//  Copyright © 2020 Matías Méndez. All rights reserved.
+//
+
+#ifndef ConexionesComputadoras_hpp
+#define ConexionesComputadoras_hpp
+
+#include <stdio.h>
+
+#endif /* ConexionesComputadoras_hpp */
